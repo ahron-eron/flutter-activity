5a228b1720ca32eb83b618dc136e73a0e8e5f009
ahron-eron <ahronval151@gmail.com>
2026-09-11 02:12:25 +0000

    First Edit

--- pogi/lib/main.dart
+++ pogi/lib/main.dart
@@ -13,7 +13,39 @@ class MyApp extends StatelessWidget{
           debugShowCheckedModeBanner: false,
           home: Scaffold(
             body: Center(
-              child: Text('hi')
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.account_circle,
+                    size: 145.0,
+                    color: Colors.deepPurple,
+                  ),
+                  Text(
+                    'Ahron Valmoria', 
+                      style: TextStyle(
+                      fontSize: 21,
+                      backgroundColor:  Colors.white,
+                      color: Colors.black,
+                    )
+                  ),
+                  Text('Pogi si sir Rodney.'),
+                  Text(
+                    'Bachelor of Science in Information Technology',
+                     style: TextStyle(
+                      fontSize: 16,
+                      backgroundColor:  Colors.white,
+                      color: Colors.black,
+                    )
+                  ),
+                  Icon(
+                    Icons.home,
+                    size: 16,
+                    color: Colors.deepPurple,
+                  )
+                ],
+              )
             ),
           ),
         );
